1cdd2a5d90518047d4bf142529868c7d77ce573b
anissaspano <90479879+anissaspano@users.noreply.github.com>
2022-03-08 00:33:14 -0500

    start attempt, change type, reveal answer pass

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,28 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [progress, setProgress] = useState<boolean>(false);
+    const [value, setValue] = useState<number>(4);
+    function flipProgress(): void {
+        setProgress(!progress);
+    }
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    setValue(value - 1);
+                    flipProgress();
+                }}
+                disabled={progress || value === 0}
+            >
+                Start Quiz
+            </Button>
+            <Button onClick={() => setValue(1 + value)} disabled={progress}>
+                Mulligan
+            </Button>
+            <Button onClick={() => flipProgress()} disabled={!progress}>
+                Stop Quiz
+            </Button>
+        </div>
+    );
 }
