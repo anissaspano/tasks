dad46feaba13d85701135d5b8c7538e53a4bd5b2
anissaspano <90479879+anissaspano@users.noreply.github.com>
2022-03-08 01:44:13 -0500

    two dice pass test

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,37 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [rightDie, rollRightDie] = useState<number>(6);
+    const [leftDie, rollLeftDie] = useState<number>(3);
+
+    const isMatching = (leftDie: number, rightDie: number): boolean => {
+        return leftDie === rightDie;
+    };
+
+    const isWinner = (leftDie: number): boolean => {
+        return leftDie !== 1;
+    };
+
+    const printStatement = (match: boolean, win: boolean): string => {
+        let message = "";
+        if (match) {
+            if (match && win) {
+                message = "Win";
+            } else {
+                message = "Lose";
+            }
+        } else {
+            message = "";
+        }
+        return message;
+    };
+    return (
+        <div>
+            <span data-testid="left-die">{leftDie}</span>.
+            <span data-testid="right-die">{rightDie}</span>.
+            <Button onClick={() => rollLeftDie(d6)}>Roll Left</Button>
+            <Button onClick={() => rollRightDie(d6)}>Roll Right</Button>
+            {printStatement(isMatching(leftDie, rightDie), isWinner(leftDie))}
+        </div>
+    );
 }
