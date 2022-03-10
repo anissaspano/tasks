63572ab4606d16c07a3d56f80f15b089282df8e1
anissaspano <90479879+anissaspano@users.noreply.github.com>
2022-03-10 11:49:01 -0500

    fixed app.tsx merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,6 @@ function App(): JSX.Element {
                 Anissa Spano UD CISC275 Starter Website
             </header>
             <hr></hr>
-<<<<<<< HEAD
-            <DoubleHalf></DoubleHalf>
-=======
             <CheckAnswer expectedAnswer="42"></CheckAnswer>
             <hr></hr>
             <GiveAttempts></GiveAttempts>
@@ -39,8 +36,7 @@ function App(): JSX.Element {
                 expectedAnswer="b"
             ></MultipleChoiceQuestion>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
->>>>>>> upstream/task-forms
+            <DoubleHalf></DoubleHalf>
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
