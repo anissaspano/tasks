2d445a875ebdd8a09809c005004901486e15ea2d
anissaspano <90479879+anissaspano@users.noreply.github.com>
2022-03-10 19:13:56 -0500

    mult choice test passes

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +11,34 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [chosenAnswer, setChosenAnswer] = useState<string>(options[0]);
+
+    function checkAnswer(expectedAnswer: string, inputAnswer: string): string {
+        let display = "❌";
+        if (expectedAnswer === inputAnswer) {
+            display = "✔️";
+        }
+        return display;
+    }
+
+    function updateAnswer(event: ChangeEvent) {
+        setChosenAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="multChoiceQuestion">
+                <Form.Label>What is the answer?</Form.Label>
+                <Form.Select value={chosenAnswer} onChange={updateAnswer}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <s>Answer is {checkAnswer(expectedAnswer, chosenAnswer)}</s>
         </div>
     );
 }
